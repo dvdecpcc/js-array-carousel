ddddc39ab1511a8151185620f089736349f72bc1
davide <dacapoccia@gmail.com>
2022-07-14 18:15:33 +0200

    finally....!

--- main.js
+++ main.js
@@ -7,21 +7,27 @@ const imagesArray =[
 ]
 
 let activeImg = 0;
+
 const imagesDom = document.querySelector('.images');
 
 for(let x = 0; x<imagesArray.length; x++){
+ 
     imagesDom.innerHTML+= `<div class="image"><img class="img" src = "${imagesArray[x]}"></div>`
+
 }
 
-const imageList = document.getElementsByClassName('image');
+
+const imageList = document.querySelectorAll('.image');
 imageList[activeImg].classList.add('show');
 
 const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
 next.addEventListener('click',
     function(){
         imageList[activeImg].classList.remove('show');
         activeImg++;
-        itemList[activeImg].classList.add('show');
+        imageList[activeImg].classList.add('show');
         prev.classList.remove('hidden');
         if (activeImg == imageList.length -1) {
             next.classList.add('hidden');
@@ -29,12 +35,17 @@ next.addEventListener('click',
     }
 )
 
+
 prev.addEventListener('click',
     function(){
+        console.log(imageList[0]);
+        console.log(activeImg);
+
         imageList[activeImg].classList.remove('show');
         activeImg--;
-        itemList[activeImg].classList.add('show');
+        imageList[activeImg].classList.add('show');
         next.classList.remove('hidden');
+
         if (activeImg == 0) {
             prev.classList.add('hidden');
         }
